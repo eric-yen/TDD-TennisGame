82fc3cd900fc771f4457fa9a54d145d707b84cf4
eric-yen <ericyen28@gmail.com>
2018-12-01 21:07:24 +0800

    合併 adv 判斷, 並加入 player name

--- tennisGame.js
+++ tennisGame.js
@@ -5,6 +5,8 @@ var tennisGame = (function() {
         2: 'Thirty',
         3: 'Forty'
     }
+    var _player1 = "Player1";
+    var _player2 = "Player2";
     var _player1Score = 0;
     var _player2Score = 0;
 
@@ -19,11 +21,8 @@ var tennisGame = (function() {
             else if ((_player1Score <= 3) && (_player2Score <= 3) && (_player1Score !== _player2Score)) {
                 return _scoreMap[_player1Score] + "-" + _scoreMap[_player2Score];
             }
-            else if ((_player1Score >= 4) && (_player1Score - _player2Score === 1)) {
-                return "Player1 Adv";
-            }
-            else if ((_player2Score >= 4) && (_player2Score - _player1Score === 1)) {
-                return "Player2 Adv";
+            else if ((_player1Score >= 4 || _player2Score >= 4) && Math.abs(_player1Score - _player2Score) === 1) {
+                return this.advancedPlayer() + " Adv";
             }
             else if ((_player1Score >= 4) && (_player1Score - _player2Score >= 2)) {
                 return "Player1 Win";
@@ -40,6 +39,10 @@ var tennisGame = (function() {
 
         setPlayer2Score: function(score) {
             _player2Score = score;
+        },
+
+        advancedPlayer: function() {
+            return ((_player1Score > _player2Score) ? _player1 : _player2);
         }
     }
 });
