281fe0ccaa773aa2cf17069edabb14017abb1a0c
eric-yen <ericyen28@gmail.com>
2018-12-01 19:30:31 +0800

    簡化 tennisGame : 加入 scoreMap

--- tennisGame.js
+++ tennisGame.js
@@ -1,15 +1,14 @@
 var tennisGame = (function() {
+    var _scoreMap = {
+        1: 'Fifteen',
+        2: 'Thirty',
+        3: 'Forty'
+    }
     var _player1Score = 0;
     return {
         score: function() {
-            if (_player1Score === 1) {
-                return "Fifteen-Love";
-            }
-            else if (_player1Score === 2) {
-                return "Thirty-Love";
-            }
-            else if (_player1Score === 3) {
-                return "Forty-Love";
+            if (_player1Score >= 1) {
+                return _scoreMap[_player1Score] + "-Love";
             }
             return "Love-All";
         },
